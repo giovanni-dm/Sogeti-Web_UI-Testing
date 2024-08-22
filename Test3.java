591964ec606e92b73bc7169b22e53e2264862631
giovanni-dm <giovanni.de.maio.langella@gmail.com>
2024-08-22 21:38:39 +0200

    Add files via upload

--- Test3.java
+++ Test3.java
@@ -16,7 +16,7 @@ public class Test3 {
         // Set up the WebDriver (ensure the ChromeDriver path is correctly set)
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Giovanni-Outlook\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-
+        driver.manage().window().maximize();
         try {
             // Navigate to the Sogeti homepage
             driver.get("https://www.sogeti.com/");
@@ -25,6 +25,7 @@ public class Test3 {
             
             long time = 3000;
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(time));
+            
             
 
             // Click on the Worldwide dropdown link
@@ -38,36 +39,48 @@ public class Test3 {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
             
             
+            
+            
             // Belgium example: wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"country-list-id\"]/ul/li[1]/a")));
             
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("country-list")));
+            //wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("country-list")));
 
             // Get all country links
             
             
             //Belgium example: List<WebElement> countryLinks = driver.findElements(By.xpath("//*[@id=\"country-list-id\"]/ul/li[1]/a"));
 
-            List<WebElement> countryLinks = driver.findElements(By.className("country-list"));
+			
+            
+            
+            List<WebElement> countryLinks = driver.findElements(By.cssSelector("#country-list-id a"));
             
             // Iterate over each link, click it, and assert it opens without errors
             for (WebElement link : countryLinks) {
                 String countryUrl = link.getAttribute("href");
-                
+                System.out.println(countryUrl);
 
-                try {
-                    driver.navigate().to(countryUrl);
+				
+				  try { driver.navigate().to(countryUrl); 
+				  
+				  
+				  // Wait for the page to load and check the title (basic assertion)
+				  
+				  System.out.println("Link working: " + countryUrl); } catch (Exception e) {
+				  System.out.println("Link broken: " + countryUrl); }
+				 
 
-                    // Wait for the page to load and check the title (basic assertion)
-                    wait.until(ExpectedConditions.titleContains("Sogeti"));
-                    System.out.println("Link working: " + countryUrl);
-                } catch (Exception e) {
-                    System.out.println("Link broken: " + countryUrl);
-                }
-
-                // Navigate back to the main page and reopen the dropdown
-                driver.navigate().back();
-                worldwideDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/div[2]/span")));
-                worldwideDropdown.click();
+				
+				  // Navigate back to the main page and reopen the dropdown
+				  
+				  driver.navigate().back();
+				  
+				  worldwideDropdown =
+				  wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+				  "//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/div[2]/span")));
+				  worldwideDropdown.click();
+				  
+				 
             }
 
         } finally {
