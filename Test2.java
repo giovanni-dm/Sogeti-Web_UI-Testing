b5913f8adb621b7da2282faa192ceb863dca343c
giovanni-dm <giovanni.de.maio.langella@gmail.com>
2024-08-22 20:56:13 +0200

    Add files via upload

--- Test2.java
+++ Test2.java
@@ -22,7 +22,7 @@ public class Test2 {
 
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        Actions actions = new Actions(driver);
+        Actions action = new Actions(driver);
         
         // Generate random data
         String randomUserFirstName = "First Name " + UUID.randomUUID().toString().substring(0, 8);
@@ -45,40 +45,51 @@ public class Test2 {
             long time = 2000;
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(time));
             
+            
 
             // 2. Hover over Services Link and Click Automation link
-            WebElement servicesLink = driver.findElement(By.xpath("//*[@id=\\\"main-menu\\\"]/ul/li[3]/div[1]"));
-            actions.moveToElement(servicesLink).perform();
-            WebElement automationLink = driver.findElement(By.xpath("//a[text()='Automation']"));
-            wait.until(ExpectedConditions.visibilityOf(automationLink));
-            automationLink.click();
+
+            WebElement servicesLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/div[1]")));
+	        action.moveToElement(servicesLink).perform();
+
+	        // Click on 'Automation' link
+	        WebElement automationLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Automation']")));
+	        action.moveToElement(automationLink).click().perform();
+	    
             
             
+
 
             // 3. Scroll to the Contact us Form
-            WebElement contactForm = driver.findElement(By.id("99a12a58-3899-4fe1-a5c7-b9065fe635b0"));
-            actions.moveToElement(contactForm).perform();
+            WebElement contactForm = driver.findElement(By.name("__field_123927"));
+            
+            action.moveToElement(contactForm).perform();
 
             // 4. Fill the form with random data
-            contactForm.findElement(By.id("4ff2ed4d-4861-4914-86eb-87dfa65876d8")).sendKeys(randomUserFirstName);
-            contactForm.findElement(By.id("11ce8b49-5298-491a-aebe-d0900d6f49a7")).sendKeys(randomUserFamilyName);
-            contactForm.findElement(By.id("056d8435-4d06-44f3-896a-d7b0bf4d37b2")).sendKeys(randomEmail);
-            contactForm.findElement(By.id("755aa064-7be2-432b-b8a2-805b5f4f9384")).sendKeys(randomPhoneNumber);
-            contactForm.findElement(By.id("message")).sendKeys("This is a test message.");
+            
+            
+            driver.findElement(By.xpath("//*[@id=\"4ff2ed4d-4861-4914-86eb-87dfa65876d8\"]")).sendKeys(randomUserFirstName);
+            driver.findElement(By.id("11ce8b49-5298-491a-aebe-d0900d6f49a7")).sendKeys(randomUserFamilyName);
+            driver.findElement(By.id("056d8435-4d06-44f3-896a-d7b0bf4d37b2")).sendKeys(randomEmail);
+            driver.findElement(By.id("755aa064-7be2-432b-b8a2-805b5f4f9384")).sendKeys(randomPhoneNumber);
+            driver.findElement(By.id("88459d00-b812-459a-99e4-5dc6eff2aa19")).sendKeys("This is a test message.");
 
             // 5. Check the I agree checkbox
-            WebElement agreeCheckbox = contactForm.findElement(By.xpath("//input[@type='checkbox']"));
-            agreeCheckbox.click();
+            WebElement agreeCheckbox = driver.findElement(By.id("__field_1239350"));
+            
+            Boolean isEnabled = agreeCheckbox.isEnabled();
+            
+            System.out.println("Checkbox is enabled: "+isEnabled);
 
             // 6. Click SUBMIT button
-            WebElement submitButton = contactForm.findElement(By.xpath("//button[@type='submit']"));
+            WebElement submitButton = driver.findElement(By.id("b35711ee-b569-48b4-8ec4-6476dbf61ef8"));
             submitButton.click();
 
             // 7. Assert Thank you message
             WebElement thankYouMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("thank-you-message")));
             assert thankYouMessage.getText().contains("Thank you");
 
-            System.out.println("Test completed successfully.");
+            System.out.println("Test completed successfully."); 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -86,4 +97,6 @@ public class Test2 {
             driver.quit();
         }
     }
+
+	
 }
